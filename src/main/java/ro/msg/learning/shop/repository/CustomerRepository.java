e7ad291a9a9db136cebb5495f36b94708e21d32a
AndreiMischie <Andrei.Mischie@msg.group>
2023-07-20 13:21:14 +0300

    created repositories and db migration with flyway

---
+++ src/main/java/ro/msg/learning/shop/repository/CustomerRepository.java
@@ -0,0 +1,6 @@
+package ro.msg.learning.shop.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ro.msg.learning.shop.entity.Customer;
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
